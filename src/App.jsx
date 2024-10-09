fa6fb4d64711bac22aa011ed799d18f2ebcefc01
Harry Newman <harrisonnewman@Harrys-MacBook-Pro.local>
2024-10-09 13:06:48 +0200

    end of class

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
-          {/* <Router path='/controller' element={<Controller/>}/> */}
+          <Route path='/controller' element={<Controller/>}/>
         </Routes>
       </Router>
     </div>
