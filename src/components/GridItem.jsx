604523e20391ba32dee0d43c36a6c9ecfce4f29b
Harry Newman <harrisonnewman@Harrys-MacBook-Pro.local>
2024-10-15 17:51:10 +0200

    transitions

--- src/components/GridItem.jsx
+++ src/components/GridItem.jsx
@@ -1,13 +1,12 @@
 import styles from "../styles/GridItem.module.css"
 
-export default function GridItem(){
+export default function GridItem() {
     return (
-
-<div>
-        <div className={styles.videoDiv}>
-            <p>Hi</p>
+        <div>
+            <div className={styles.videoDiv}>
+                
+            </div>
         </div>
-    </div>
     )
-    
+
 }
