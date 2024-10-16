e69ea51fa69c1ec6aa9fa682c22fdb9bfd6defd0
Harrison Newman <harrydnewman@gmail.com>
2024-10-16 19:07:00 +0200

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Controller from './pages/Controller';
+import ScrollHolder from './pages/ScrollHolder'; // Adjust path if needed
 import Grid from './pages/Grid';
+import About from './pages/About'; // Adjust path if needed
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/controller' element={<Controller/>}/>
-          <Route path='/gridtest' element={<Grid/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/controller" element={<Controller />} />
+          <Route path="/scroll" element={<Controller showNavbar={true}><ScrollHolder /></Controller>} />
+          <Route path="/grid" element={<Controller showNavbar={true}><Grid /></Controller>} />
+          <Route path="/about" element={<Controller showNavbar={true}><About /></Controller>} />
         </Routes>
       </Router>
     </div>
--- src/pages/Controller.jsx
+++ src/pages/Controller.jsx
@@ -1,63 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from '../components/Navbar';
-import styles from '../styles/Controller.module.css'
-import ScrollHolder from './ScrollHolder';
-import Grid from './Grid';
-import About from './About';
+import styles from '../styles/Controller.module.css';
 
-export default function Controller() {
-    const [currentSelection, setCurrentSelection] = useState('scroll');
-
-
-    const handleSelectionChange = (newSelection) => {
-        console.log("Current Selection:", newSelection)
-        setCurrentSelection(newSelection);
-        
-        // Additional logic can be added here
-    };
-
-    if (currentSelection === 'scroll'){
-        return (
+export default function Controller({ showNavbar, children }) {
+    return (
         <div>
-            <div className={styles.controllerDiv}>
-                <div className={styles.navbarDiv}>
-                    <Navbar onSelectionChange={handleSelectionChange} />
-                </div>
-                </div>
-            <div className={styles.mainPageDiv}>
-                <ScrollHolder/>
-            </div>
-        </div> 
-         );
-    }
-    else if (currentSelection === 'grid'){
-        return (
-            <div>
+            {showNavbar && (
                 <div className={styles.controllerDiv}>
                     <div className={styles.navbarDiv}>
-                        <Navbar onSelectionChange={handleSelectionChange} />
+                        <Navbar />
                     </div>
-                    </div>
-                <div className={styles.mainPageDiv}>
-                    <Grid/>
                 </div>
-            </div> 
-             );
-    }
-    else {
-        return (
-        <div>
-        <div className={styles.controllerDiv}>
-            <div className={styles.navbarDiv}>
-                <Navbar onSelectionChange={handleSelectionChange} />
+            )}
+            <div className={styles.mainPageDiv}>
+                {children}
             </div>
-            </div>
-        <div className={styles.mainPageDiv}>
-            <About/>
         </div>
-    </div> 
-     );
-    }
+    );
 }
-
-
